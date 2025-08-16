8db71a1fc4a64f431dda89d381188f668986f0ad
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-08-16 20:20:42 +0530

    Add files via upload

---
+++ week1/day1/FirstProgram.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FirstProgram {
+	/**
+	 * 
+	 * shortcut for main mthd:type main and do ctrl+space
+	 * main mthd--->entry point for the execution of the pgm.
+	 * 
+	 * shortcut for sysout--->type syso and do ctrl+space
+	 */
+	
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to testleaf");
+		
+		
+	}
+
+}
---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		//primitive datatypes
+		byte noOfLearners=60;
+		short regNo=12345;
+		long phNo=9874546677L;
+		int LicenseNO=12345678;
+		
+		float speed=0.0276767678787876767676f;
+		double capacity=134.66762877676;
+		System.out.println(speed+"\\n"+capacity);
+		//System.out.println(capacity);
+		
+		char logo='S';
+		char num='5';
+		boolean type=true;
+		
+		//non primitive 
+		String str="p67627878296387hghffdh";
+		String s="hello everyone welcome to testleaf for you first session";
+		
+		
+
+	}
+
+}
